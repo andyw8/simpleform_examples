8825e57fc388c240f867d73bcb238ed132ebfa48
Andy Waite <github.aw@andywaite.com>
2013-01-03 11:00:19 +0000

    New Rails project
    $ rails new simpleform_examples --skip-sprockets --skip-javascript --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleformExamples::Application.config.secret_token = '39cb468419063136ba7ebe1f19c373031ee4db265e299d922f9b802fe070fed4df0bbbbf0f9374fe1bcb80e21aeed363ceeca26dd449f70b8a2be97fa9e69814'
