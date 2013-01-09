086c059d9a8b86481e5eb1ad3780082eebc2ce0d
Andy Waite <github.aw@andywaite.com>
2013-01-09 11:12:40 +0000

    Accessors

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,11 +1,10 @@
 class ProductCategory < ActiveRecord::Base
+  attr_accessible :product
+  attr_accessible :category
+
   belongs_to :product
   belongs_to :category
 
   validates_associated :product
   validates_associated :category
-
-  # TODO work out why this causes ProductsController#create to fail
-  # validates :product, presence: true
-  # validates :category, presence: true
 end
